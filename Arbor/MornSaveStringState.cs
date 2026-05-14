121c8b2155c5eff9366668e507c5addeb3622cb7
matsufriends <pikapikan0526@gmail.com>
2026-05-14 21:42:53 +0900

    fix: USE_MORNSTATE 優先 / どちらもない場合はクラス除外 + MornState 参照追加

--- Arbor/MornSaveStringState.cs
+++ Arbor/MornSaveStringState.cs
@@ -1,5 +1,10 @@
-#if USE_ARBOR
+#if USE_MORNSTATE || USE_ARBOR
+#if USE_MORNSTATE
+using MornLib;
+using StateBehaviour = MornLib.MornStateBehaviour;
+#elif USE_ARBOR
 using Arbor;
+#endif
 using System;
 using UnityEngine;
 using VContainer;
@@ -20,4 +25,4 @@ namespace MornLib
         }
     }
 }
-#endif
+#endif // USE_MORNSTATE || USE_ARBOR
